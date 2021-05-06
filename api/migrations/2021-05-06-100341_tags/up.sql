22b300f1d943e5a0369a734ace62bb823b192b3d
makotia <3mdevmail@gmail.com>
2021-05-06 21:46:59 +0900

    Revert "add tags table"
    This reverts commit ad6c352ccef05c92e688f75bd8bda32f12f6298f.

--- api/migrations/2021-05-06-100341_tags/up.sql
+++
@@ -1,7 +0,0 @@
--- Your SQL goes here
-CREATE TABLE tags (
-  id int not null auto_increment,
-  title varchar(20) not null,
-  icon text,
-  primary key (id)
-) ENGINE=InnoDB;
--- api/src/schema.rs
+++ api/src/schema.rs
@@ -5,16 +5,3 @@ table! {
         created_at -> Datetime,
     }
 }
-
-table! {
-    tags (id) {
-        id -> Integer,
-        title -> Varchar,
-        icon -> Nullable<Text>,
-    }
-}
-
-allow_tables_to_appear_in_same_query!(
-    posts,
-    tags,
-);
