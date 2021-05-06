638cc10afdbc660b7b7d7479d622e0c01cbec781
makotia <3mdevmail@gmail.com>
2021-05-06 19:27:00 +0900

    add post_tags table

--- api/src/schema.rs
+++ api/src/schema.rs
@@ -7,6 +7,14 @@ table! {
 }
 
 table! {
+    post_tags (id) {
+        id -> Integer,
+        tag_id -> Integer,
+        post_id -> Integer,
+    }
+}
+
+table! {
     tags (id) {
         id -> Integer,
         title -> Varchar,
@@ -14,7 +22,11 @@ table! {
     }
 }
 
+joinable!(post_tags -> posts (post_id));
+joinable!(post_tags -> tags (tag_id));
+
 allow_tables_to_appear_in_same_query!(
     posts,
+    post_tags,
     tags,
 );
