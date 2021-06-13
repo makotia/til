38a5b8596f098e691e15cb38a2f4dc0813dcafa3
makotia <3mdevmail@gmail.com>
2021-06-13 17:15:24 +0900

    fix build

--- front/src/lib/token.ts
+++ front/src/lib/token.ts
@@ -1,11 +1,11 @@
 export const getToken: () => string | null = () => {
-  return localStorage.getItem("TOKEN")
+  return typeof window !== "undefined" ? localStorage.getItem("TOKEN") : ""
 }
 
 export const setToken: (token: string) => void = token => {
-  localStorage.setItem("TOKEN", token)
+  typeof window !== "undefined" && localStorage.setItem("TOKEN", token)
 }
 
 export const clearToken: () => void = () => {
-  localStorage.removeItem("TOKEN")
+  typeof window !== "undefined" && localStorage.removeItem("TOKEN")
 }
