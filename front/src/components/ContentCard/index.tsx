2e6aeebbaf694c8eee5a2000b8c53854e07a8d71
makotia <3mdevmail@gmail.com>
2021-06-13 17:04:31 +0900

    add content page

--- front/src/components/ContentCard/index.tsx
+++ front/src/components/ContentCard/index.tsx
@@ -1,8 +1,12 @@
-import {h, FunctionComponent} from "preact"
+import { h, FunctionComponent } from "preact"
 
+import axios from "axios"
+import { useState } from "preact/hooks"
 import { style } from "typestyle"
 
+import { BASE_URL } from "../../consts"
 import { formatJA, strToDayjs } from "../../lib/date"
+import { getToken } from "../../lib/token"
 import { Content } from "../../types"
 
 const rootStyle = style({
@@ -16,11 +20,43 @@ const dateStyle = style({
   fontSize: "12px",
 })
 
+type AddContentProps = {
+  postId: string;
+  contentId?: string;
+  refetchFunc: (id: string) => void;
+}
+
 const ContentCard: FunctionComponent<Content> = ({ content, created_at }: Content) => {
   return (
     <div className={rootStyle}>
       <p className={dateStyle}>{formatJA(strToDayjs(created_at))}</p>
       <p>{content}</p>
+    </div>
+  )
+}
+
+export const AddContent: FunctionComponent<AddContentProps> = (props) => {
+  const [content, setContent] = useState<string>("")
+  const token = getToken()
+  const submit = () => {
+    axios.post<Content>(
+      `${BASE_URL}/posts/${props.postId}/contents`,
+      { content },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      .then(() => {
+        props.refetchFunc(props.postId)
+        setContent("")
+      })
+      .catch(err => console.error(err))
+  }
+  return (
+    <div className={rootStyle}>
+      <textarea value={content} onInput={e => setContent(e.currentTarget.value)} />
+      <button onClick={submit}>投稿</button>
     </div>
   )
 }
